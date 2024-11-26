a7343f5a5c7c6d4e5a21d5c30461e3deb4d3b34c
waycsj <waycsj@gmail.com>
2024-11-26 13:53:01 +0900

    second commit

---
+++ src/main/java/org/example/cicdtest1/Cicdtest1Application.java
@@ -0,0 +1,13 @@
+package org.example.cicdtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdtest1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cicdtest1Application.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/cicdtest1/Cicdtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdtest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdtest1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
